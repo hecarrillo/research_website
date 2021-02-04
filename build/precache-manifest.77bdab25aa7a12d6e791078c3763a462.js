c5ce12bb1c1a217a6bb59fd2cd5370c93f48a277
Hector Carrillo <technik.und.spiel@gmail.com>
2021-02-04 12:42:09 -0600

    pre github pages

---
+++ build/precache-manifest.77bdab25aa7a12d6e791078c3763a462.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3549b81af89fa2759d9a652664aac8b5",
+    "url": "/research_website/index.html"
+  },
+  {
+    "revision": "bde6c788555e53bac9d4",
+    "url": "/research_website/static/css/main.c5d5fb42.chunk.css"
+  },
+  {
+    "revision": "b4fc10c4297c0126a018",
+    "url": "/research_website/static/js/2.3ea5c924.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/research_website/static/js/2.3ea5c924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bde6c788555e53bac9d4",
+    "url": "/research_website/static/js/main.913ec60f.chunk.js"
+  },
+  {
+    "revision": "a14c0806036b87ac0db4",
+    "url": "/research_website/static/js/runtime-main.f23d89d6.js"
+  }
+]);
