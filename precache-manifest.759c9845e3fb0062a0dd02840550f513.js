84ffc570aa525885cbb7c74f6c0b11112e81a0fb
Hector Carrillo <technik.und.spiel@gmail.com>
2021-02-21 16:44:52 -0600

    Updates

--- precache-manifest.759c9845e3fb0062a0dd02840550f513.js
+++ precache-manifest.759c9845e3fb0062a0dd02840550f513.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/research_website/static/js/runtime~main.9fbcc537.js"
   },
   {
-    "revision": "bcaf18a00b85d2279641",
-    "url": "/research_website/static/js/main.bcaf18a0.chunk.js"
+    "revision": "bf1e1ba54b7214817e30",
+    "url": "/research_website/static/js/main.bf1e1ba5.chunk.js"
   },
   {
     "revision": "0d2058fb72bc8e7470a7",
     "url": "/research_website/static/js/1.0d2058fb.chunk.js"
   },
   {
-    "revision": "bcaf18a00b85d2279641",
+    "revision": "bf1e1ba54b7214817e30",
     "url": "/research_website/static/css/main.4de0ef8e.chunk.css"
   },
   {
-    "revision": "5bbe5845c807dbf4ffb6836d109c6e1e",
+    "revision": "71cdc3ce0227a0dba83179e1f357f244",
     "url": "/research_website/index.html"
   }
 ];
