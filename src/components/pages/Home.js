28b3844455c6a249c7721681e60b59fb47cf3826
gwitius <0215602@up.edu.mx>
2021-02-25 10:00:36 -0600

    change html

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -14,12 +14,12 @@ function Home() {
       <div className='body'>
         <h1>Abstract</h1>
         <p>Why do people decide to undertake businesses? 
-          This is an important inquiry nowadays, and a question young people 
+          This topic is important nowadays, and a question young people 
           should start asking themselves. Instead, questions like: Why do I need to start a 
           business? Do I need a lot of knowledge? Am I capable enough? among many others, are the ones 
-          stopping many people from taking theit first step towards starting a new business.<br/>
+          stopping many people from taking their first step towards starting a new business.<br/>
           The core problem is that most people do not even understand the concept of
-          entrepreneurship; thus, failing to carry out their ideas.<br/>
+          entrepreneurship and, in consequence, fail to carry out their ideas.<br/>
           Entrepreneurship ranges from selling candies at your school to creating a product 
           capable of beating all competition and selling it to multi-billion dollar companies. According 
           to the RAE, the term entrepreneuring means: "to undertake and start a work, a business, 
